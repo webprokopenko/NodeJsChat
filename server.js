56986fbf7f61f829cdee598610ab312d8a61ec1d
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-24 13:03:19 +0300

    Globals require

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ var Vasya = new user.User("Vasya");
 var Petya = new user.User("Petya");
 
 Vasya.hello(Petya);
+
+require ('./global_user.js');
+
+var GlobalVasya = new User("Global vasya");
+var GlobalPetya = new User("Global petya");
+GlobalVasya.hello(GlobalPetya);
