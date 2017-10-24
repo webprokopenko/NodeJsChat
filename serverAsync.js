fd377db02dfc2b353edd16d9b430437da37b8953
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-24 21:43:35 +0300

    fs Async read file

---
+++ serverAsync.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+
+/**
+ * 1 Блокирует. Использовать там где нет паралельных процессов
+ * 2 Работает try catch
+ * 3 Прост и понятен
+ */
+http.createServer(function(req,res){
+    var info;
+
+    if(req.url == '/'){
+        
+            info = fs.readFile('inde.html',function(err,info){
+                if(err){
+                    console.log(err);
+                    res.statusCode = 500;
+                    res.end("На сервере произошла ошибка");
+                    return;
+                }
+                res.end(info);
+            });
+    }else {
+        /**
+         * 404
+         */
+    }
+}).listen(3000);
