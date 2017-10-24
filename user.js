7bbebc73e5c788b2def532da98346d4246305e14
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-24 13:07:54 +0300

    Modules require *.json

--- user.js
+++ user.js
@@ -1,8 +1,10 @@
+var phrases = require("./ru.json");
+
 function User(name){
     this.name = name;
 }
 User.prototype.hello = function(who){
-    console.log("Hello " + who.name);
+    console.log(phrases.Hello + who.name);
 }
 console.log("User Js is required");
 
