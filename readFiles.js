d8ffd4e09969512b994b3aba5ab0c84c15a65ada
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-25 13:11:31 +0300

    Read files ASync error message

--- readFiles.js
+++ readFiles.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 
-fs.readFile(__filename,{encoding:"utf8"}, function(err,data){
-    if(err)
-        console.log(err);
+fs.readFile("blablalba",{encoding:"utf8"}, function(err,data){
+    if(err.code = "ENOENT")
+        console.log(err.message);
     else
         console.log(data);
 })
