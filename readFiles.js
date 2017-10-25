84c2a2e3265d817171c1847d6bd3b360e5c8944c
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-25 13:18:37 +0300

    Read files ASync fs.stat fs.stat.isFile()

--- readFiles.js
+++ readFiles.js
@@ -1,5 +1,10 @@
 var fs = require('fs');
 
+fs.stat(__filename, function(err,stat){
+    console.log(stat.isFile());
+    console.log(stat);
+});
+
 fs.readFile("blablalba",{encoding:"utf8"}, function(err,data){
     if(err.code = "ENOENT")
         console.log(err.message);
